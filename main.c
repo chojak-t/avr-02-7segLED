934fbf70f945171ccd54d0d51b5eab7b5dbef073
chojak-t <chojak.t@gmail.com>
2016-07-17 19:08:12 +0200

    Initial settings

---
+++ main.c
@@ -0,0 +1,56 @@
+/*
+ * main.c
+ *
+ *  Created on: 17 lip 2016
+ *      Author: Tomasz Chojak
+ *
+ * 7 segment LED with common annode as output at PORTC
+ * Remember about resistors attached to all segments.
+ *
+ *	*** segments a-g ***
+ *		 a
+ * 		 --
+ * 	   f|  |b
+ * 	     --g
+ * 	   e|  |c
+ * 	     --
+ * 	     d
+ *
+ * *** PORTC as output ***
+ * PC0 -> a
+ * PC1 -> b
+ * PC2 -> c
+ * PC3 -> d
+ * PC4 -> e
+ * PC5 -> f
+ * PC6 -> g
+ *
+ * *** which segment should be on to display a digth? ***
+ * 0 -> abcdef
+ * 1 -> bc
+ * 2 -> abged
+ * 3 -> abgcd
+ * 4 -> fgbc
+ * 5 -> afgcd
+ * 6 -> afedcg
+ * 7 -> abcf
+ * 8 -> abcdefg
+ * 9 -> abcdfg
+ *
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define SEG_A (1<<0)
+#define SEG_B (1<<0)
+#define SEG_C (1<<0)
+#define SEG_D (1<<0)
+#define SEG_E (1<<0)
+#define SEG_F (1<<0)
+#define SEG_G (1<<0)
+
+int main(void)
+{
+	PORTC = 0xFF; //PORTC as output - leds
+}
